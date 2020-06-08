4b4b4596520f02bf910e7db703b9f16f5d57ebac
Alexey Chernyshov <artifixer@outlook.com>
2020-06-08 23:37:15 +0300

    Mercenary leave score

---
+++ Patches/GetScoreOfMercenaryToLeaveKingdom.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using System;
+using System.Reflection;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents.Map;
+
+namespace AllegianceOverhaul.Patches
+{
+  [HarmonyPatch(typeof(DefaultDiplomacyModel), "GetScoreOfMercenaryToLeaveKingdom")]
+  public class GetScoreOfMercenaryToLeaveKingdomPatch
+  {
+    public static void Postfix(Clan mercenaryClan, Kingdom kingdom, DefaultDiplomacyModel __instance, ref float __result)
+    {
+      try
+      {
+        if (!SettingsHelper.InDebugBranch || !Settings.Instance.EnableTechnicalDebugging || !SettingsHelper.FactionInScope(mercenaryClan, Settings.Instance.EnsuredLoyaltyDebugScope))
+          return;
+
+        float ComputedResult = (float)(10000.0 * (0.00999999977648258 * (double)Math.Min(100f, mercenaryClan.LastFactionChangeTime.ElapsedDaysUntilNow)) - 5000.0) - __instance.GetScoreOfMercenaryToJoinKingdom(mercenaryClan, kingdom);
+
+        string UnitValueDebugInfo = String.Format("ScoreOfMercenaryToLeaveKingdom. DaysWithFaction = {0}. DaysWithFactionModified = {1}. GetScoreOfMercenaryToJoinKingdom = {2}." +
+          "CalculatedResult = {3}. NativeResult = {4}.",
+          mercenaryClan.LastFactionChangeTime.ElapsedDaysUntilNow.ToString(), (10000.0 * (0.00999999977648258 * (double)Math.Min(100f, mercenaryClan.LastFactionChangeTime.ElapsedDaysUntilNow))).ToString("N"),
+          __instance.GetScoreOfMercenaryToJoinKingdom(mercenaryClan, kingdom).ToString("N"),
+          ComputedResult.ToString("N"), __result.ToString("N"));
+
+        MessageHelper.TechnicalMessage(UnitValueDebugInfo);
+      }
+      catch (Exception ex)
+      {
+        MethodInfo methodInfo = MethodBase.GetCurrentMethod() as MethodInfo;
+        DebugHelper.HandleException(ex, methodInfo, "Harmony patch for GetScoreOfMercenaryToLeaveKingdom");
+      }
+    }
+    public static bool Prepare()
+    {
+      return Settings.Instance.EnableTechnicalDebugging;
+    }
+  }
+}
